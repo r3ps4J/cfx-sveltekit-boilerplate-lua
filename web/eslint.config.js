1645f2ed429df8664a6b720c540a3038fccb6546
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-08-20 20:31:07 +0200

    Turn off @typescript-eslint/no-explicit-any

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -18,6 +18,9 @@ export default [
                 ...globals.node,
             },
         },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+        },
     },
     {
         files: ["**/*.svelte"],
